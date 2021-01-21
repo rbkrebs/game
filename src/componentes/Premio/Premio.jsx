a94107af3a05723bc5f399ca8fb6538e5ef12c1f
Romulo Krebs <romulo.krebs@gmail.com>
2021-01-20 22:04:06 -0300

    ajusta path de figuras premio

--- src/componentes/Premio/Premio.jsx
+++ src/componentes/Premio/Premio.jsx
@@ -20,20 +20,20 @@ export default function Premio(){
            <p className="premio__mensagem">Parabéns, você foi excelente! Você ganhou muitos pontos! Continue se capacitando...</p>
            <div className="premio__badges">
             <div className="premio__badge">
-                <img src={pensamentocritico}
+                <img src={process.env.PUBLIC_URL+pensamentocritico}
                  alt= {`badget de ${pensamentocritico}`}></img>
                 <span>Pensamento crítico</span>
                 <span>3000 pontos</span>
             </div>
             
             <div className="premio__badge">
-                <img src={resolucaodeproblemas}
+                <img src={process.env.PUBLIC_URL+resolucaodeproblemas}
                 alt= {`badget de ${resolucaodeproblemas}`}></img>
                 <span>Resolução de problema</span>
                 <span>1505 pontos</span>
             </div>
             <div className="premio__badge">
-                <img src={criatividade}
+                <img src={process.env.PUBLIC_URL+criatividade}
                  alt= {`badget de ${criatividade}`}></img>
                 <span>Criatividade</span>
                 <span>4620 pontos</span>
