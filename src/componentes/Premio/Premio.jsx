753f29c21e8976a7bcc1cd1141380a67ffb8dac2
Romulo Krebs <romulo.krebs@gmail.com>
2021-01-20 22:22:27 -0300

    ajusta path de imagens de premio

--- src/componentes/Premio/Premio.jsx
+++ src/componentes/Premio/Premio.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import criatividade from '../../assets/softskills/criatividade.png'
-import resolucaodeproblemas from '../../assets/softskills/resolucaodeproblemas.png'
-import pensamentocritico from '../../assets/softskills/pensamentocritico.png'
+
 
 
 
@@ -20,21 +18,24 @@ export default function Premio(){
            <p className="premio__mensagem">Parabéns, você foi excelente! Você ganhou muitos pontos! Continue se capacitando...</p>
            <div className="premio__badges">
             <div className="premio__badge">
-                <img src={process.env.PUBLIC_URL+pensamentocritico}
-                 alt= {`badget de ${pensamentocritico}`}></img>
+                <img src={`${process.env.PUBLIC_URL+
+                '../../assets/softskills/pensamentocritico.png'}`}
+                 alt= "badget de pensamento crítico"></img>
                 <span>Pensamento crítico</span>
                 <span>3000 pontos</span>
             </div>
             
             <div className="premio__badge">
-                <img src={process.env.PUBLIC_URL+resolucaodeproblemas}
-                alt= {`badget de ${resolucaodeproblemas}`}></img>
+            <img src={`${process.env.PUBLIC_URL+
+            '../../assets/softskills/resolucaodeproblemas.png'}`}
+                alt= "badget de resolução de problema"></img>
                 <span>Resolução de problema</span>
                 <span>1505 pontos</span>
             </div>
             <div className="premio__badge">
-                <img src={process.env.PUBLIC_URL+criatividade}
-                 alt= {`badget de ${criatividade}`}></img>
+            <img src={`${process.env.PUBLIC_URL+
+            '../../assets/softskills/criatividade.png'}`}
+                 alt= "badget de criatividade"></img>
                 <span>Criatividade</span>
                 <span>4620 pontos</span>
             </div>  
