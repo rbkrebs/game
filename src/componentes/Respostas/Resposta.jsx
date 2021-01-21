ca2624734cc813504a5ac703bc41a5238a32f7ee
Romulo Krebs <romulo.krebs@gmail.com>
2021-01-20 22:10:13 -0300

    ajusta path de setas respostas

--- src/componentes/Respostas/Resposta.jsx
+++ src/componentes/Respostas/Resposta.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import setaDefault from '../../assets/cenarios/arrowdefault.png';
-import setaHover from '../../assets/cenarios/arrowhover.png';
+import setaDefault from process.env.PUBLIC_URL+'../../assets/cenarios/arrowdefault.png';
+import setaHover from process.env.PUBLIC_URL+'../../assets/cenarios/arrowhover.png';
 import { useSelector } from 'react-redux'
 
 
@@ -26,8 +26,7 @@ export default function Resposta({resposta, handleClick}) {
             onClick={() => handleClick(resposta.id)}>
 
             <img src={clicked === resposta.id ? 
-            process.env.PUBLIC_URL+setaHover : 
-            process.env.PUBLIC_URL+setaDefault}
+            setaHover : setaDefault}
             
             alt="seta para direita" />
 
